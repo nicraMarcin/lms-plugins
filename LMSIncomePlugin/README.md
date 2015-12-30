8456497e78c5914af5f96e5941f447596b4c0357
Łukasz Kopiszka <lukasz@alfa-system.pl>
2015-12-30 09:52:02 +0100

    Income raport - init plugin

---
+++ LMSIncomePlugin/README.md
@@ -0,0 +1,11 @@
+# LMSIncomePlugin
+
+Zbiercze przestawienie przychodu z podziałem na dni w ujęciu miesięcznym oraz na miesiące w ujęciu rocznym.
+
+## Instalacja 
+
+* Aktywuj plugin w zakładce `Konfiguracja => Wtyczki`
+
+## Uwagi
+
+* Stara prowizorka przepisana na plugin nie odpowiadam za poniesione straty moralne :)
---
+++ LMSIncomePlugin/lib/locale/pl/strings.php
@@ -0,0 +1,11 @@
+<?php
+
+$_LANG['Plugin Example'] = 'Przykładowa wtyczka';
+
+$_LANG['day'] = 'dzień';
+$_LANG['month'] = 'miesiąc';
+$_LANG['income'] = 'przychód';
+$_LANG['Revenues of days in a month'] = 'Przychody według dni w miesiącu';
+$_LANG['Revenue by months of the year'] = 'Przychody według miesięcy w roku';
+
+?>
