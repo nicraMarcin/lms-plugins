93129279ced3ba55ef9c218358b4d2d7937a7721
Łukasz Kopiszka (kyob) <lukasz@alfa-system.pl>
2016-07-08 13:20:20 +0200

    template support

--- LMSIncomePlugin/handlers/IncomeHandler.php
+++ LMSIncomePlugin/handlers/IncomeHandler.php
@@ -1,9 +1,5 @@
 <?php
 
-/**
- * IncomeHandler
- *
- */
 class IncomeHandler
 {
 
@@ -24,8 +20,11 @@ class IncomeHandler
     public function smartyIncome(Smarty $hook_data)
     {
         $template_dirs = $hook_data->getTemplateDir();
-        $plugin_templates = PLUGINS_DIR . DIRECTORY_SEPARATOR . LMSIncomePlugin::PLUGIN_DIRECTORY_NAME . DIRECTORY_SEPARATOR . 'templates';
-        array_unshift($template_dirs, $plugin_templates);
+        $plugin_templates = PLUGINS_DIR . DIRECTORY_SEPARATOR . LMSCustomersAgePlugin::PLUGIN_DIRECTORY_NAME . DIRECTORY_SEPARATOR . 'templates';
+        $custom_templates_dir = ConfigHelper::getConfig('phpui.custom_templates_dir');
+        if (!empty($custom_templates_dir) && file_exists($plugin_templates . DIRECTORY_SEPARATOR . $custom_templates_dir) && !is_file($plugin_tempaltes . DIRECTORY_SEPARATOR . $custom_templates_dir)) {
+            $plugin_templates = PLUGINS_DIR . DIRECTORY_SEPARATOR . LMSCustomersAgePlugin::PLUGIN_DIRECTORY_NAME . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . $custom_templates_dir;
+        } array_unshift($template_dirs, $plugin_templates);
         $hook_data->setTemplateDir($template_dirs);
         return $hook_data;
     }
@@ -36,6 +35,5 @@ class IncomeHandler
         array_unshift($hook_data, $plugin_modules);
         return $hook_data;
     }
-
 
 }
