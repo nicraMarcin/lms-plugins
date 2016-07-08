e4e637cdac0dc78ea52d7beec6718fc52d678bcc
Łukasz Kopiszka (kyob) <lukasz@alfa-system.pl>
2016-07-08 13:17:51 +0200

    template support

--- LMSCashImportOKBSPlugin/handlers/CashImportOKBSHandler.php
+++ LMSCashImportOKBSPlugin/handlers/CashImportOKBSHandler.php
@@ -1,14 +1,8 @@
 <?php
 
-/**
- * CashImportOKBSHandler
- *
- */
-class CashImportOKBSHandler
-{
+class CashImportOKBSHandler {
 
-    public function menuCashImportOKBS(array $hook_data = array())
-    {
+    public function menuCashImportOKBS(array $hook_data = array()) {
         $cashimportokbs_submenus = array(
             array(
                 'name' => trans('Import OKBS'),
@@ -21,21 +15,21 @@ class CashImportOKBSHandler
         return $hook_data;
     }
 
-    public function smartyCashImportOKBS(Smarty $hook_data)
-    {
+    public function smartyCashImportOKBS(Smarty $hook_data) {
         $template_dirs = $hook_data->getTemplateDir();
-        $plugin_templates = PLUGINS_DIR . DIRECTORY_SEPARATOR . LMSCashImportOKBSPlugin::PLUGIN_DIRECTORY_NAME . DIRECTORY_SEPARATOR . 'templates';
-        array_unshift($template_dirs, $plugin_templates);
+        $plugin_templates = PLUGINS_DIR . DIRECTORY_SEPARATOR . LMSCustomersAgePlugin::PLUGIN_DIRECTORY_NAME . DIRECTORY_SEPARATOR . 'templates';
+        $custom_templates_dir = ConfigHelper::getConfig('phpui.custom_templates_dir');
+        if (!empty($custom_templates_dir) && file_exists($plugin_templates . DIRECTORY_SEPARATOR . $custom_templates_dir) && !is_file($plugin_tempaltes . DIRECTORY_SEPARATOR . $custom_templates_dir)) {
+            $plugin_templates = PLUGINS_DIR . DIRECTORY_SEPARATOR . LMSCustomersAgePlugin::PLUGIN_DIRECTORY_NAME . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . $custom_templates_dir;
+        } array_unshift($template_dirs, $plugin_templates);
         $hook_data->setTemplateDir($template_dirs);
         return $hook_data;
     }
 
-    public function modulesDirCashImportOKBS(array $hook_data = array())
-    {
+    public function modulesDirCashImportOKBS(array $hook_data = array()) {
         $plugin_modules = PLUGINS_DIR . DIRECTORY_SEPARATOR . LMSCashImportOKBSPlugin::PLUGIN_DIRECTORY_NAME . DIRECTORY_SEPARATOR . 'modules';
         array_unshift($hook_data, $plugin_modules);
         return $hook_data;
     }
-
 
 }
