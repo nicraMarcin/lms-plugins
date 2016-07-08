fe81483bb3debbe1b149e4e3993521d9954034f4
Łukasz Kopiszka (kyob) <lukasz@alfa-system.pl>
2016-07-08 15:06:53 +0200

    plugin template support and bclean template

--- ClipsPlugin/ClipsPlugin.php
+++ ClipsPlugin/ClipsPlugin.php
@@ -1,42 +1,14 @@
 <?php
 
-/*
- *  LMS version 1.11-git
- *
- *  Copyright (C) 2001-2013 LMS Developers
- *
- *  Please, see the doc/AUTHORS for more information about authors!
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License Version 2 as
- *  published by the Free Software Foundation.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
- *  USA.
- *
- *  $Id$
- */
-
-/**
- * ClipsPlugin
- * 
- * @author Łukasz Kopiszka <lukasz@alfa-system.pl>
- */
 class ClipsPlugin extends LMSPlugin {
-	const PLUGIN_NAME = 'ClipsPlugin';
-	const PLUGIN_DESCRIPTION = 'Integracja LMS z CLIPS w SmartEdge.';
-	const PLUGIN_AUTHOR = 'Łukasz Kopiszka &lt;lukasz@alfa-system.pl&gt;';
-	const PLUGIN_DIRECTORY_NAME = 'ClipsPlugin';
 
-    public function registerHandlers()
-    {
+    const PLUGIN_NAME = 'ClipsPlugin';
+    const PLUGIN_DESCRIPTION = 'Integracja LMS z CLIPS w SmartEdge.';
+    const PLUGIN_AUTHOR = 'Łukasz Kopiszka &lt;lukasz@alfa-system.pl&gt;';
+    const PLUGIN_DIRECTORY_NAME = 'ClipsPlugin';
+    const PLUGIN_TEMPLATE_NAME = 'bclean';
+
+    public function registerHandlers() {
         $this->handlers = array(
             'smarty_initialized' => array(
                 'class' => 'ClipsHandler',
@@ -46,10 +18,11 @@ class ClipsPlugin extends LMSPlugin {
                 'class' => 'ClipsHandler',
                 'method' => 'modulesDirClips'
             ),
-	    'nodeinfo_before_display' => array (
-		'class' => 'ClipsHandler',
-		'method' => 'nodeInfoBeforeDisplay'
-	    )
+            'nodeinfo_before_display' => array(
+                'class' => 'ClipsHandler',
+                'method' => 'nodeInfoBeforeDisplay'
+            )
         );
     }
+
 }
--- ClipsPlugin/handlers/ClipsHandler.php
+++ ClipsPlugin/handlers/ClipsHandler.php
@@ -133,11 +133,11 @@ class ClipsHandler {
 
     public function smartyClips(Smarty $hook_data) {
         $template_dirs = $hook_data->getTemplateDir();
-        $plugin_templates = PLUGINS_DIR . DIRECTORY_SEPARATOR . LMSCustomersAgePlugin::PLUGIN_DIRECTORY_NAME . DIRECTORY_SEPARATOR . 'templates';
-        $custom_templates_dir = ConfigHelper::getConfig('phpui.custom_templates_dir');
-        if (!empty($custom_templates_dir) && file_exists($plugin_templates . DIRECTORY_SEPARATOR . $custom_templates_dir) && !is_file($plugin_tempaltes . DIRECTORY_SEPARATOR . $custom_templates_dir)) {
-            $plugin_templates = PLUGINS_DIR . DIRECTORY_SEPARATOR . LMSCustomersAgePlugin::PLUGIN_DIRECTORY_NAME . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . $custom_templates_dir;
-        } array_unshift($template_dirs, $plugin_templates);
+        $plugin_templates = PLUGINS_DIR . DIRECTORY_SEPARATOR . ClipsPlugin::PLUGIN_DIRECTORY_NAME . DIRECTORY_SEPARATOR . 'templates';
+        if (!empty(ClipsPlugin::PLUGIN_TEMPLATE_NAME) && file_exists($plugin_templates . DIRECTORY_SEPARATOR . ClipsPlugin::PLUGIN_TEMPLATE_NAME) && !is_file($plugin_templates . DIRECTORY_SEPARATOR . ClipsPlugin::PLUGIN_TEMPLATE_NAME)) {
+            $plugin_templates = PLUGINS_DIR . DIRECTORY_SEPARATOR . ClipsPlugin::PLUGIN_DIRECTORY_NAME . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . ClipsPlugin::PLUGIN_TEMPLATE_NAME;
+        }
+        array_unshift($template_dirs, $plugin_templates);
         $hook_data->setTemplateDir($template_dirs);
         return $hook_data;
     }
@@ -175,5 +175,7 @@ class ClipsHandler {
         }
         return $hook_data;
     }
+
 }
+
 ?>
