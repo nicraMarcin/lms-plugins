83ddc4094c749ae20782e59781806475100b4cd5
Łukasz Kopiszka (kyob) <lukasz@alfa-system.pl>
2016-07-08 13:18:40 +0200

    template support

--- LMSCustomersAgePlugin/handlers/CustomersAgeHandler.php
+++ LMSCustomersAgePlugin/handlers/CustomersAgeHandler.php
@@ -1,14 +1,8 @@
 <?php
 
-/**
- * CustomersAgeHandler
- *
- */
-class CustomersAgeHandler
-{
+class CustomersAgeHandler {
 
-    public function menuCustomersAge(array $hook_data = array())
-    {
+    public function menuCustomersAge(array $hook_data = array()) {
         $customersage_submenus = array(
             array(
                 'name' => trans('Customers age'),
@@ -21,21 +15,21 @@ class CustomersAgeHandler
         return $hook_data;
     }
 
-    public function smartyCustomersAge(Smarty $hook_data)
-    {
+    public function smartyCustomersAge(Smarty $hook_data) {
         $template_dirs = $hook_data->getTemplateDir();
         $plugin_templates = PLUGINS_DIR . DIRECTORY_SEPARATOR . LMSCustomersAgePlugin::PLUGIN_DIRECTORY_NAME . DIRECTORY_SEPARATOR . 'templates';
-        array_unshift($template_dirs, $plugin_templates);
+        $custom_templates_dir = ConfigHelper::getConfig('phpui.custom_templates_dir');
+        if (!empty($custom_templates_dir) && file_exists($plugin_templates . DIRECTORY_SEPARATOR . $custom_templates_dir) && !is_file($plugin_tempaltes . DIRECTORY_SEPARATOR . $custom_templates_dir)) {
+            $plugin_templates = PLUGINS_DIR . DIRECTORY_SEPARATOR . LMSCustomersAgePlugin::PLUGIN_DIRECTORY_NAME . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . $custom_templates_dir;
+        } array_unshift($template_dirs, $plugin_templates);
         $hook_data->setTemplateDir($template_dirs);
         return $hook_data;
     }
 
-    public function modulesDirCustomersAge(array $hook_data = array())
-    {
+    public function modulesDirCustomersAge(array $hook_data = array()) {
         $plugin_modules = PLUGINS_DIR . DIRECTORY_SEPARATOR . LMSCustomersAgePlugin::PLUGIN_DIRECTORY_NAME . DIRECTORY_SEPARATOR . 'modules';
         array_unshift($hook_data, $plugin_modules);
         return $hook_data;
     }
-
 
 }
