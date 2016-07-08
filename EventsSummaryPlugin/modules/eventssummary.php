1584df83d735103d9f630088788a4fb592546d26
Łukasz Kopiszka (kyob) <lukasz@alfa-system.pl>
2016-07-08 12:31:05 +0200

    replace global with instance

--- EventsSummaryPlugin/modules/eventssummary.php
+++ EventsSummaryPlugin/modules/eventssummary.php
@@ -1,8 +1,12 @@
 <?php
 
+$layout['pagetitle'] = trans('Events summary');
+
 function getEventsToCloseWithoutAssign() {
-    global $LMS, $SMARTY, $DB, $SESSION;
-    $eventsNoAssign = $DB->GetAll('SELECT FROM_UNIXTIME(e.date, \'%Y-%m-%d\') AS startdate, e.title, e.id, u.name
+    global $LMS, $SMARTY, $SESSION;
+    $db = LMSDB::getInstance();
+
+    $eventsNoAssign = $db->GetAll('SELECT FROM_UNIXTIME(e.date, \'%Y-%m-%d\') AS startdate, e.title, e.id, u.name
 				    FROM events e 
 				    LEFT JOIN users u ON (u.id=e.userid) 
 				    WHERE e.id NOT IN (SELECT eventid FROM eventassignments) AND e.closed=0');
@@ -10,8 +14,10 @@ function getEventsToCloseWithoutAssign() {
 }
 
 function getSummary() {
-    global $LMS, $SMARTY, $DB, $SESSION;
-    $summary = $DB->GetAll('SELECT u.id, u.name, COUNT(e.closed) AS wszystkie, SUM(IF(e.closed, 1, 0)) AS zamkniete, SUM(IF(e.closed, 0, 1)) AS otwarte
+    global $LMS, $SMARTY, $SESSION;
+    $db = LMSDB::getInstance();
+
+    $summary = $db->GetAll('SELECT u.id, u.name, COUNT(e.closed) AS wszystkie, SUM(IF(e.closed, 1, 0)) AS zamkniete, SUM(IF(e.closed, 0, 1)) AS otwarte
 			    FROM eventassignments ea
 			    LEFT JOIN users u ON (u.id = ea.userid)
 			    LEFT JOIN events e ON (e.id = ea.eventid)
