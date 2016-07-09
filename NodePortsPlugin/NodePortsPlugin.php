af6c23ee19acad8020f383c09ab1c4d21338828e
Łukasz Kopiszka (kyob) <lukasz@alfa-system.pl>
2016-07-09 09:39:26 +0200

    add plugin template path

--- NodePortsPlugin/NodePortsPlugin.php
+++ NodePortsPlugin/NodePortsPlugin.php
@@ -1,51 +1,29 @@
 <?php
 
-/*
- *  LMS version 1.11-git
- *
- *  Copyright (C) 2001-2013 LMS Developers
- *
- *  Please, see the doc/AUTHORS for more information about authors!
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License Version 2 as
- *  published by the Free Software Foundation.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
- *  USA.
- *
- *  $Id$
- */
-
 /**
  * NodePortsPlugin
- * 
+ *
  * @author Łukasz Kopiszka <lukasz@alfa-system.pl>
  */
 class NodePortsPlugin extends LMSPlugin {
-	const PLUGIN_NAME = 'NodePortsPlugin';
-	const PLUGIN_DESCRIPTION = 'Dodatkowe odnośniki przy IP dla różnych usług.';
-	const PLUGIN_AUTHOR = 'Łukasz Kopiszka &lt;lukasz@alfa-system.pl&gt;';
-	const PLUGIN_DIRECTORY_NAME = 'NodePortsPlugin';
 
-    public function registerHandlers()
-    {
+    const PLUGIN_NAME = 'NodePortsPlugin';
+    const PLUGIN_DESCRIPTION = 'Dodatkowe odnośniki przy IP dla różnych usług.';
+    const PLUGIN_AUTHOR = 'Łukasz Kopiszka &lt;lukasz@alfa-system.pl&gt;';
+    const PLUGIN_DIRECTORY_NAME = 'NodePortsPlugin';
+    const PLUGIN_TEMPLATE_NAME = 'bclean';
+
+    public function registerHandlers() {
         $this->handlers = array(
             'smarty_initialized' => array(
                 'class' => 'NodePortsHandler',
                 'method' => 'smartyNodePorts'
             ),
-	    'nodeinfo_before_display' => array (
-		'class' => 'NodePortsHandler',
-		'method' => 'nodeInfoBeforeDisplay'
-	    )
+            'nodeinfo_before_display' => array(
+                'class' => 'NodePortsHandler',
+                'method' => 'nodeInfoBeforeDisplay'
+            )
         );
     }
+
 }
